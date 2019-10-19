6ca5a6a6fd03d25394c73206a3fcb23b7c4bb707
MakiseYuki <31904890+MakiseYuki@users.noreply.github.com>
2019-10-20 02:45:25 +0900

    Q2 rn

--- Exam 1/Exam1Question2.m
+++ Exam 1/Exam1Question2.m
@@ -1,5 +1,5 @@
 clear all;
-xt = 0.5; yt = 0.2;
+xt = 0.9; yt = 0.9;
 object_true = [xt yt]';
 
 for sample = 1:4
@@ -36,17 +36,23 @@ for sample = 1:4
     [X,Y] = meshgrid(x,y);
 
     g = zeros(length(x),length(y));
+    kg = zeros(length(x),length(y),k);
+    
     g = double(g);
 
     for i = 1:length(x)
         for j = 1:length(y)
-            point = [X(i,j) Y(i,j)]';
-            es_r = distance(point,land_mark)';
-            g(i,j) = estMAP(r,true_distance,Sigma_noise,Sigma_x,Sigma_y,point);
+                point = [X(i,j) Y(i,j)]';
+                es_r = distance(point,land_mark)';
+                gs = estMAP(es_r,true_distance,Sigma_noise,Sigma_x,Sigma_y,point);
+                for z = 1:k
+                    kg(i,j,z) = gs(z);
+                end
+                g(i,j) = min(gs);
         end
     end
 
-
+    
 
 
     % x = linspace(-2,2);
@@ -60,8 +66,18 @@ for sample = 1:4
     plot(land_mark(1,:),land_mark(2,:),'or'); axis equal, hold on, axis([-2 2 -2 2]);
     contour(X,Y,g,20);
     %contour(X,Y,Z);
+    
 end
 
+    figure(2),
+    plot(xt,yt,'+g'); axis equal, hold on
+    plot(land_mark(1,:),land_mark(2,:),'or'); axis equal, hold on, axis([-2 2 -2 2]);
+    contour(X,Y,kg(:,:,1)); hold on
+    contour(X,Y,kg(:,:,2)); hold on
+    contour(X,Y,kg(:,:,3)); hold on
+    contour(X,Y,kg(:,:,4)); hold on
+    
+    
 function gm = estMAP(r,mu,Sigma_noise,Sigma_x,Sigma_y,point)
 %     L = (r-mu)^2/Sigma_noise^2;
 %     P = (x^2/Sigma_x^2) + (y^2/Sigma_y^2);
@@ -69,9 +85,10 @@ function gm = estMAP(r,mu,Sigma_noise,Sigma_x,Sigma_y,point)
     
     p = (point(1)^2/Sigma_x^2) + (point(2)^2/Sigma_y^2);
     of =(r-mu).^2/Sigma_noise^2;
-    g = p + of;
-    gm = min(g);
+    gm = p + of;
+    %gm = min(g);
 end
+
 
 function dis = distance(object_true,k)
     dis = sqrt((object_true(1)-k(1,:)).^2+(object_true(2)-k(2,:)).^2);
--- Exam 1/Test.m
+++ Exam 1/Test.m
@@ -1,18 +1,5 @@
 clear all;
 
-land_mark = [0 0]';
-x = linspace(-2,2);
-y = linspace(-2,2);
-[X,Y] = meshgrid(x,y);
-temp = zeros(length(x),length(y));
-for i = 1:length(x)
-        for j = 1:length(y)
-            point = [X(i,j) Y(i,j)];
-            temp(i,j) = distance(point,land_mark);
-        end
-end
+x = mvnrnd(3,0.3,100);
 
-
-function dis = distance(object_true,k)
-    dis = sqrt((object_true(1)-k(1,:)).^2+(object_true(2)-k(2,:)).^2);
-end
+plot(x);
