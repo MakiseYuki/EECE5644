a3a95b5bc57c8ef7cdd26f7ea6f478c1f41e81b5
MakiseYuki <31904890+MakiseYuki@users.noreply.github.com>
2019-11-24 05:22:59 +0900

    exam2 Q1 p2

--- Exam 2/Exam2Q1.m
+++ Exam 2/Exam2Q1.m
@@ -1,59 +1,53 @@
 clear all, close all,
 
 plotData = 1;
-n = 3; Ntrain = 1000; 
-alpha = [0.15,0.25,0.30,0.30]; % must add to 1.0
-meanVectors = [-10 -10 10 10;-10 10 -10 10;10 10 10 10];
-covEvalues = [3.2^2 0 0;0 0.6^2 0;0 0 1.2^2];
-covEvectors(:,:,1) =  0.7*[3 5 4;-2 10 9;1 2 4]/sqrt(2);
-covEvectors(:,:,2) =  0.7*[3 5 4;-2 10 9;1 2 4]/sqrt(2);
-covEvectors(:,:,3) =  0.7*[3 5 4;-2 10 9;1 2 4]/sqrt(2);
-covEvectors(:,:,4) =  0.7*[3 5 4;-2 10 9;1 2 4]/sqrt(2);
+n = 3; Ntrain = 1000; Ntest = 10000; 
+alpha = [0.23,0.27,0.18,0.32]; % must add to 1.0
+meanVectors = [-10 -8 9 12;-11 9 -9 10;12 13 11 10];
+covEvalues = [3.2^2 0 0;0 1.2^2 0;0 0 0.6^2];
+covEvectors(:,:,1) =  0.5*[2 7 4;-2 15 9;1 2 3]/sqrt(2);
+covEvectors(:,:,2) =  0.5*[2 7 4;-2 15 9;1 2 3]/sqrt(2);
+covEvectors(:,:,3) =  0.5*[2 7 4;-2 15 9;1 2 3]/sqrt(2);
+covEvectors(:,:,4) =  0.5*[2 7 4;-2 15 9;1 2 3]/sqrt(2);
 
 t = rand(1,Ntrain);
 ind1 = find(0 <= t & t <= alpha(1));
 ind2 = find(alpha(1) < t & t <= alpha(1)+alpha(2));
-ind3 = find(alpha(1)+alpha(2) <= t & t <= alpha(1)+alpha(2)+alpha(3));
-ind4 = find(alpha(1)+alpha(2)+alpha(3) <= t & t <= 1);
+ind3 = find(alpha(1)+alpha(2) < t & t <= alpha(1)+alpha(2)+alpha(3));
+ind4 = find(alpha(1)+alpha(2)+alpha(3) < t & t <= 1);
 Xtrain = zeros(n,Ntrain);
-Ltrain = zeros(1,Ntrain);
 Xtrain(:,ind1) = covEvectors(:,:,1)*covEvalues^(1/2)*randn(n,length(ind1))+meanVectors(:,1);
-Ltrain(ind1) = 1;
 Xtrain(:,ind2) = covEvectors(:,:,2)*covEvalues^(1/2)*randn(n,length(ind2))+meanVectors(:,2);
-Ltrain(ind2) = 2;
 Xtrain(:,ind3) = covEvectors(:,:,3)*covEvalues^(1/2)*randn(n,length(ind3))+meanVectors(:,3);
-Ltrain(ind3) = 3;
 Xtrain(:,ind4) = covEvectors(:,:,4)*covEvalues^(1/2)*randn(n,length(ind4))+meanVectors(:,4);
-Ltrain(ind4) = 4;
 
-% MAP classify
-Ltrain_classified = MAP_Classifier(Xtrain,meanVectors,covEvectors,alpha,4);
+trueLabel = zeros(1,Ntrain);
+trueLabel(:,ind1) = 1;
+trueLabel(:,ind2) = 2;
+trueLabel(:,ind3) = 3;
+trueLabel(:,ind4) = 4;
+
+MAPLabel_classified = MAP_Classifier(Xtrain,meanVectors,covEvectors,alpha,4);
+
 % Calculate the misclassified samples
-ind12 = find(Ltrain_classified==2 & Ltrain==1);
-ind13 = find(Ltrain_classified==3 & Ltrain==1);
-ind14 = find(Ltrain_classified==4 & Ltrain==1);
-ind21 = find(Ltrain_classified==1 & Ltrain==2);
-ind23 = find(Ltrain_classified==3 & Ltrain==2);
-ind24 = find(Ltrain_classified==4 & Ltrain==2);
-ind31 = find(Ltrain_classified==1 & Ltrain==3);
-ind32 = find(Ltrain_classified==2 & Ltrain==3);
-ind34 = find(Ltrain_classified==4 & Ltrain==3);
-ind41 = find(Ltrain_classified==1 & Ltrain==4);
-ind42 = find(Ltrain_classified==2 & Ltrain==4);
-ind43 = find(Ltrain_classified==3 & Ltrain==4);
-Pe1 = (size(ind12,2)+size(ind13,2)+size(ind14,2))/size(ind1,2)
-Pe2 = (size(ind21,2)+size(ind23,2)+size(ind24,2))/size(ind2,2)
-Pe3 = (size(ind31,2)+size(ind32,2)+size(ind34,2))/size(ind3,2)
-Pe4 = (size(ind41,2)+size(ind42,2)+size(ind43,2))/size(ind4,2)
-% % Save Xtrain dataset
-% fid = fopen('Xtrain_1000_3.txt','wt');
-% fid2 = fopen('Ltrain_1000_3.txt','wt');
-% fprintf(fid,'%g %g %g\n',Xtrain(1,:),Xtrain(2,:),Xtrain(3,:)); 
-% fprintf(fid2,'%g\n',Ltrain); 
-% fclose(fid);
-% fclose(fid2);
+ind12 = find(MAPLabel_classified==2 & trueLabel==1);
+ind13 = find(MAPLabel_classified==3 & trueLabel==1);
+ind14 = find(MAPLabel_classified==4 & trueLabel==1);
+ind21 = find(MAPLabel_classified==1 & trueLabel==2);
+ind23 = find(MAPLabel_classified==3 & trueLabel==2);
+ind24 = find(MAPLabel_classified==4 & trueLabel==2);
+ind31 = find(MAPLabel_classified==1 & trueLabel==3);
+ind32 = find(MAPLabel_classified==2 & trueLabel==3);
+ind34 = find(MAPLabel_classified==4 & trueLabel==3);
+ind41 = find(MAPLabel_classified==1 & trueLabel==4);
+ind42 = find(MAPLabel_classified==2 & trueLabel==4);
+ind43 = find(MAPLabel_classified==3 & trueLabel==4);
 
-% Draw Xtrain and misclassified samples
+pError1 = (size(ind12,2)+size(ind13,2)+size(ind14,2))/size(ind1,2);
+pError2 = (size(ind21,2)+size(ind23,2)+size(ind24,2))/size(ind2,2);
+pError3 = (size(ind31,2)+size(ind32,2)+size(ind34,2))/size(ind3,2);
+pError4 = (size(ind41,2)+size(ind42,2)+size(ind43,2))/size(ind4,2);
+
 figure(2), subplot(2,2,1),
 plot3(Xtrain(1,:),Xtrain(2,:),Xtrain(3,:),'.g'),hold on,
 plot3(Xtrain(1,ind12),Xtrain(2,ind12),Xtrain(3,ind12),'xb'),hold on,
@@ -92,7 +86,29 @@ if plotData == 1
     plot3(Xtrain(1,ind4),Xtrain(2,ind4),Xtrain(3,ind4),'.m'),
     title('Training Data'), axis equal,
 end
-% 
-% 
+
+t = rand(1,Ntest);
+ind1 = find(0 <= t & t <= alpha(1));
+ind2 = find(alpha(1) < t & t <= alpha(1)+alpha(2));
+ind3 = find(alpha(1)+alpha(2) < t & t <= alpha(1)+alpha(2)+alpha(3));
+ind4 = find(alpha(1)+alpha(2)+alpha(3) < t & t <= 1);
+Xtest = zeros(n,Ntrain);
+Xtest(:,ind1) = covEvectors(:,:,1)*covEvalues^(1/2)*randn(n,length(ind1))+meanVectors(:,1);
+Xtest(:,ind2) = covEvectors(:,:,2)*covEvalues^(1/2)*randn(n,length(ind2))+meanVectors(:,2);
+Xtest(:,ind3) = covEvectors(:,:,3)*covEvalues^(1/2)*randn(n,length(ind3))+meanVectors(:,3);
+Xtest(:,ind4) = covEvectors(:,:,4)*covEvalues^(1/2)*randn(n,length(ind4))+meanVectors(:,4);
 
 
+
+
+
+
+
+% if plotData == 1
+%     figure(1), subplot(1,2,1),
+%     plot(Xtrain(1,:),Xtrain(2,:),'.')
+%     title('Training Data'), axis equal,
+%     subplot(1,2,2),
+%     plot(Xtest(1,:),Xtest(2,:),'.')
+%     title('Testing Data'), axis equal,
+% end
